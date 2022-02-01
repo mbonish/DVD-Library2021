38c9a2192d3a3efa654e6277c7d812276f91336a
Mariana Bonish <51426161+mbonish@users.noreply.github.com>
2022-01-31 21:41:33 -0600

    Update App.java

--- src/main/java/DVDLib/App.java
+++ src/main/java/DVDLib/App.java
@@ -1,10 +1,20 @@
 package DVDLib;
 
+import DVDLib.UI.DVDLibView;
+import DVDLib.UI.UserIO;
+import DVDLib.UI.UserIOConsoleImpl;
 import DVDLib.controller.DVDLibController;
+import DVDLib.dao.DVDLibDao;
+import DVDLib.dao.DVDLibFileDaoImpl;
 
 public class App {
     public static void main(String[] args) {
-        DVDLibController controller = new DVDLibController();
+        UserIO myIO = new UserIOConsoleImpl();
+        DVDLibView myView = new DVDLibView(myIO);
+        DVDLibDao myDao = new DVDLibFileDaoImpl();
+        DVDLibController controller = new DVDLibController(
+                myDao, myView);
         controller.run();
+         }
     }
-}
+
