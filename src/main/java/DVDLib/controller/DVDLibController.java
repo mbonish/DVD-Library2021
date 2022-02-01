d3e2ccc70b9183d94469ddbaa54e1087a3723cea
Mariana Bonish <51426161+mbonish@users.noreply.github.com>
2022-01-31 21:41:15 -0600

    Update DVDLibController.java

--- src/main/java/DVDLib/controller/DVDLibController.java
+++ src/main/java/DVDLib/controller/DVDLibController.java
@@ -4,7 +4,7 @@ import DVDLib.UI.DVDLibView;
 import DVDLib.UI.UserIO;
 import DVDLib.UI.UserIOConsoleImpl;
 import DVDLib.dao.DVDLibDao;
-import DVDLib.dao.DVDLibFileDaoImpl;
+import DVDLib.dao.DVDLibException;
 import DVDLib.dto.DVD;
 
 import java.util.List;
@@ -12,75 +12,95 @@ import java.util.List;
 public class DVDLibController {
 
     private final UserIO io = new UserIOConsoleImpl();
-    private final DVDLibView view = new DVDLibView();
-    private final DVDLibDao dao = new DVDLibFileDaoImpl();
+    private DVDLibView view;
+    private DVDLibDao dao;
+
+    public DVDLibController(DVDLibDao dao, DVDLibView view){
+        this.dao = dao;
+        this.view = view;
+    }
 
 
     public void run() {
         boolean keepGoing = true;
         int menuSelection = 0;
-        while(keepGoing){
-        menuSelection = getMenuSelection();
 
-            switch(menuSelection){
-                case 1:
-                    listDVDs();
-                    break;
-                case 2:
-                    createDVD();
-                    break;
-                case 3:
-                    viewDVD();
-                    break;
-                case 4:
-                    editDVD();
-                    break;
-                case 5:
-                    removeDVD();
-                    break;
-                case 6:
-                    keepGoing = false;
-                    break;
+        try {
+            while (keepGoing) {
+                menuSelection = getMenuSelection();
+
+                switch (menuSelection) {
+                    case 1:
+                        listDVDs();
+                        break;
+                    case 2:
+                        createDVD();
+                        break;
+                    case 3:
+                        viewDVD();
+                        break;
+                    case 4:
+                        editDVD();
+                        break;
+                    case 5:
+                        removeDVD();
+                        break;
+                    case 6:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
             }
+            exitMessage();
+        } catch (DVDLibException e){
+            view.displayErrorMessage(e.getMessage());
         }
-        io.print("Good bye!! :)");
     }
 
     private int getMenuSelection(){
         return view.printMenuAndGetSelection();
     }
 
-    private void createDVD(){
+    private void createDVD() throws DVDLibException {
         view.displayCreateDVDBanner();
         DVD newDvd = view.getNewDVDInfo();
         dao.addDVD(newDvd);
         view.displayCreateSuccessBanner();
     }
 
-    private void listDVDs(){
+    private void listDVDs() throws DVDLibException {
         view.displayAllBanner();
         List<DVD> DVDList = dao.getAllDVDs();
         view.displayDVDList(DVDList);
     }
 
-    private void viewDVD(){
+    private void viewDVD() throws DVDLibException {
         view.displayDisplayDVDBanner();
         int DVDId = view.getDVDIdChoice();
         DVD dvd = dao.getDVD(DVDId);
         view.displayDVD(dvd);
     }
-    private void removeDVD(){
+    private void removeDVD() throws DVDLibException {
         view.displayRemoveDVDBanner();
         int DVDId = view.getDVDIdChoice();
         DVD removedDVD = dao.removeDVD(DVDId);
         view.displayRemoveResult(removedDVD);
     }
 
-    private void editDVD(){
+    private void editDVD() throws DVDLibException {
         view.displayEditDVDBanner();
         int DVDId = view.getDVDIdChoice();
         DVD editedDVD = dao.getDVD(DVDId);
         view.displayEditedDVD(editedDVD);
         view.displayDVD(editedDVD);
+    }
+
+    private void unknownCommand(){
+        view.displayUnknownCommandBanner();
+    }
+
+    private void exitMessage(){
+        view.displayExitBanner();
     }
 }
